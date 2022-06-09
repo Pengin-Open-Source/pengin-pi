55e610da1b20dba1ed0cf639c64e470d70550fa1
Kabir Campwala <kabircampwala3@gmail.com>
2022-06-09 20:53:48 +0530

    created routes for blog posts

---
+++ app/blogPosts.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, redirect, flash, url_for, request, abort
+from flask_login import login_required, current_user
+from . import db
+# todo from models import Post
+# todo from forms import postForm
+
+
+blogPosts = Blueprint('blogPosts', __name__)
+
+@blogPosts.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    return render_template('create_post.html')
+
+# todo create "postForm" in forms.py
+# todo create "Post" class in models.py that will contain id, title, author, date_posted, and content.  
+
+@blogPosts.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@blogPosts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = postForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@blogPosts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
