aca3302e7d05bc297fe2b81a3f7dd0d885e71e26
kiserd <kiserd@oregonstate.edu>
2022-06-25 14:04:40 -0700

    removed UPDATE and DELETE functionality from both templates and route handlers

--- app/blogPosts.py
+++ app/blogPosts.py
@@ -18,7 +18,7 @@ def get_links():
 @blogPosts.route("/blog")
 def display_blog_home():
     posts = BlogPost.query.limit(15)
-    return render_template('blog_main.html', posts=posts, links=get_links())
+    return render_template('blog/blog.html', posts=posts, links=get_links())
 
 
 @blogPosts.route("/blog/<int:post_id>")
@@ -54,34 +54,39 @@ def create_post():
 
 # Logan Kiser: Kabir had added some UPDATE & DELETE handlers. I tidied them
 #              up a tiny bit, but they aren't necessarily as robust as we
-#              would like them to be in production. This was outside the scope
-#              of the GH issue.
-@blogPosts.route("/blog/<int:post_id>/update", methods=['GET', 'POST'])
-# Logan Kiser: hang tight on requiring credentials for flask-principal branch
+#              would like them to be in production.
+#
+#              Per Stuart's GitHub comment expressing concern exposing
+#              DELETE/UPDATE endpoints to other request methods, will hold
+#              this off for a dedicated issue/branch. UPDATE and DELETE
+#              methods probably require some AJAX. My work to do "just enough"
+#              to get this working was getting out of hand. I will wait until
+#              I can collaborate with the front-end team
+
+# @blogPosts.route("/blog/<int:post_id>/update", methods=['GET', 'POST'])
 # @login_required
-def update_post(post_id):
-    post = BlogPost.query.get_or_404(post_id)
-    if request.method == 'POST':
-        # first retrieve object from DB
-        updated_post = BlogPost.query.get(post_id)
-        # update appropriate fields
-        # this assumes all fields are passed via request
-        updated_post.title = request.form.get('title')
-        updated_post.content = request.form.get('content')
-        updated_post.tags = request.form.get('tags')
-        db.session.commit()
-        flash('Your post has been updated!', 'success')
-        return redirect(url_for('blogPosts.display_post', post_id=post.id))
-    # populate form with existing data and allow user to alter blog post
-    return render_template('blog/update.html', post=post, links=get_links())
+# def update_post(post_id):
+#     post = BlogPost.query.get_or_404(post_id)
+#     if request.method == 'POST':
+#         # first retrieve object from DB
+#         updated_post = BlogPost.query.get(post_id)
+#         # update appropriate fields
+#         # this assumes all fields are passed via request
+#         updated_post.title = request.form.get('title')
+#         updated_post.content = request.form.get('content')
+#         updated_post.tags = request.form.get('tags')
+#         db.session.commit()
+#         flash('Your post has been updated!', 'success')
+#         return redirect(url_for('blogPosts.display_post', post_id=post.id))
+#     # populate form with existing data and allow user to alter blog post
+#     return render_template('blog/update.html', post=post, links=get_links())
 
 
-@blogPosts.route("/blog/<int:post_id>/delete", methods=['GET'])
-# Logan Kiser: hang tight on requiring credentials for flask-principal branch
-# @login_required
-def delete_post(post_id):
-    post = BlogPost.query.get_or_404(post_id)
-    db.session.delete(post)
-    db.session.commit()
-    flash('Your post has been deleted!', 'success')
-    return redirect(url_for('blogPosts.display_blog_home'))
+# @blogPosts.route("/blog/<int:post_id>/delete", methods=['DELETE'])
+# # @login_required
+# def delete_post(post_id):
+#     post = BlogPost.query.get_or_404(post_id)
+#     db.session.delete(post)
+#     db.session.commit()
+#     flash('Your post has been deleted!', 'success')
+#     return render_template('blog_main.html', links=get_links())
