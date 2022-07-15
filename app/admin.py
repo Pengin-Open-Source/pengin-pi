f3fab620d75a6bb51747df16392635cad0813487
stuart <stuart@tobupengin.com>
2022-07-15 12:23:37 +0900

    fixed user role admin view

--- app/admin.py
+++ app/admin.py
@@ -11,13 +11,21 @@ admin_bpt = Blueprint('admin_bpt', __name__)
 
 admin_permission = Permission(RoleNeed('admin'))
 
+
 class SecureModelView(ModelView):
     @admin_permission.require()
     def is_accessible(self):
         return current_user.is_authenticated
 
+
+class FixView(SecureModelView):
+    column_display_pk = True # optional, but I like to see the IDs in the list
+    column_hide_backrefs = False
+    column_list = ('id', 'user_id', 'role_id')
+
+
 #add DB model views into flask admin
 admin.add_view(SecureModelView(models.User, db.session))
 admin.add_view(SecureModelView(models.Role, db.session))
-admin.add_view(SecureModelView(models.UserRoles, db.session))
+admin.add_view(FixView(models.UserRoles, db.session))
    
