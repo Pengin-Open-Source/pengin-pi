42181793fffe27de335307356aab9b07dbc70f96
Lokesh <lokeshnarasani@gmail.com>
2022-07-20 18:03:44 -0500

    Forum post data model

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-#import flask module
+# import flask module
 from flask import Blueprint, render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user
 from . import db
@@ -6,57 +6,63 @@ from .models import User
 
 main = Blueprint('main', __name__)
 
-#dummy variable list of dictionaries 
+# dummy variable list of dictionaries
 posts = [
-        {
-            'author': 'Sergey Astvatsaturov', 
-            'title': 'Single life!',
-            'content': 'I rock', 
-            'date_posted': 'February 10th, 2022'
-        },
+    {
+        'author': 'Sergey Astvatsaturov',
+        'title': 'Single life!',
+        'content': 'I rock',
+        'date_posted': 'February 10th, 2022'
+    },
 
-        {
-            'author': 'Stuart Anderson', 
-            'title': 'Life in Japan',
-            'content': "Can't wait to get back to the states", 
-            'date_posted': 'February 11th, 2022'
-        },
-          {
-            'author': 'Dante Samuels', 
-            'title': 'Kid 2',
-            'content': 'So excited!', 
-            'date_posted': 'February 13th, 2022'
-        }
+    {
+        'author': 'Stuart Anderson',
+        'title': 'Life in Japan',
+        'content': "Can't wait to get back to the states",
+        'date_posted': 'February 11th, 2022'
+    },
+    {
+        'author': 'Dante Samuels',
+        'title': 'Kid 2',
+        'content': 'So excited!',
+        'date_posted': 'February 13th, 2022'
+    }
 
 
 ]
 
-#Routes are what you type into your browser to go to different webpages
-#use route decorators 
-#the "/" is the homepage
+# Routes are what you type into your browser to go to different webpages
+# use route decorators
+# the "/" is the homepage
+
+
 @main.route("/")
-#also alows you to go to home page
-#one function handles two seperate routes
+# also alows you to go to home page
+# one function handles two seperate routes
 @main.route("/home")
 def home():
-    #use render_template to return the home.html file 
+    # use render_template to return the home.html file
     # posts = posts gives you access to the data in that variable in your template
     return render_template('home.html')
 
 
-#create about page
+# create about page
 @main.route("/about")
 def about():
-    return render_template('about.html', title ='about')
+    return render_template('about.html', title='about')
 
-#create products page
+# create products page
+
+
 @main.route("/products")
 def products():
-    return render_template('products.html', title ='products')
+    return render_template('products.html', title='products')
+
 
 @main.route("/forums")
 def forums():
-    return render_template('forums.html', title ='forums', posts = posts)
+    return render_template('forums.html', title='forums', posts=posts)
+
 
 @main.route('/profile')
 @login_required
@@ -64,8 +70,7 @@ def profile():
     return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
 
 
-# TODO finish profile editing feature / test that it works
-#edit profile info post
+# edit profile info post
 @main.route('/profile/edit_profile', methods=['POST'])
 @login_required
 def edit_profile_post():
@@ -79,15 +84,46 @@ def edit_profile_post():
     user.name = name
     user.email = email
     db.session.commit()
-    
+
     # reload profile page
     return redirect(url_for('main.profile'))
+
 
 @main.route('/edit_password')
 def edit_password():
     return render_template('edit_password.html')
 
+
 @main.route('/edit_password', methods=['POST'])
 def edit_password_post():
     return redirect(url_for('main.profile'))
-    
+
+
+# company info
+@main.route('/company_info')
+@login_required
+def company_info():
+    return render_template('company_info/company_info.html')
+    # TODO add parameters based on company info
+
+
+@main.route('/company_info/edit_company_info', methods=['POST'])
+@login_required
+def edit_company_info_post():
+
+    # Get new information
+
+    name = request.form.get('name')
+    address1 = request.form.get('address1')
+    address1 = request.form.get('address2')
+    city = request.form.get('city')
+    state = request.form.get('state')
+    zipcode = request.form.get('zipcode')
+    country = request.form.get('country')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+    members = request.form.get('members')
+
+    # TODO reflect changes
+
+    return redirect(url_for('main.company_info'))
