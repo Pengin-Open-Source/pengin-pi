6e7916b65e9f09193fd4fe1a1dd315245c40608a
bmeissn2 <55253640+bmeissn2@users.noreply.github.com>
2022-07-21 18:37:54 +0900

    75 edit password needs to function (#102)
    * edit password button now toggles edit password form
    
    * edit password functional with some error handling
    
    * comment out alerts in layout, added comments
    
    * comments in profile.html about editing password
    
    * edit_profile.html comment
    
    * todo comment edit password
    
    * deleted duplicate div tag
    
    * fixed changing password even with wrong current password
    
    * split edit password into multiple functions
    
    Co-authored-by: achen236 <57299247+achen236@users.noreply.github.com>
    Co-authored-by: Stuart Anderson <58710710+stuart909@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 #import flask module
 from flask import Blueprint, render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models import User
 
@@ -64,7 +65,6 @@ def profile():
     return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
 
 
-# TODO finish profile editing feature / test that it works
 #edit profile info post
 @main.route('/profile/edit_profile', methods=['POST'])
 @login_required
@@ -72,22 +72,58 @@ def edit_profile_post():
     old_email = request.form.get('old_email')
     name = request.form.get('name')
     email = request.form.get('email')
-    # find user
+    # Find user
     user = User.query.filter_by(email=old_email).first()
 
-    # update name
+    # Update name
     user.name = name
     user.email = email
     db.session.commit()
     
-    # reload profile page
-    return redirect(url_for('main.profile'))
+    # Reload profile page
+    return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
 
-@main.route('/edit_password')
-def edit_password():
-    return render_template('edit_password.html')
+#edit password post
+#TODO: Password validation and requirements and check for empty inputs
 
-@main.route('/edit_password', methods=['POST'])
+@main.route('/profile/edit_password', methods=['POST'])
+@login_required
 def edit_password_post():
-    return redirect(url_for('main.profile'))
+    email = request.form.get('email')
+    curr_password = request.form.get('curr_password')
+    new_password = request.form.get('new_password')
+    confirm_new_password = request.form.get('confirm_new_password')
+    
+    # Find user
+    user = User.query.filter_by(email=email).first()
+
+    # Check if current password is correct
+    def check_curr_password(func):
+        def wrapper():
+            if not check_password_hash(user.password, curr_password):
+                # Flash error
+                flash('curr_password_error')
+                return False
+            return func()
+        return wrapper
+
+    # Check if new password and confirmation new password are the same
+    @check_curr_password
+    def check_confirm_new_password():
+        if new_password == confirm_new_password:
+            return True
+        else:
+            # Flash error
+            flash('new_password_confirm_error')
+            return False
+
+    # Hash and save new password
+    def set_new_password():
+        user.password = generate_password_hash(new_password, method='sha256')
+        db.session.commit()
+    
+    if check_confirm_new_password():
+        set_new_password()
+        
+    return redirect(url_for('main.profile')) # reload the page
     
