58fac66f8798be2b6c150b5b2c18d8e965f4b2d9
Jasmine Manansala <65846682+jmm28@users.noreply.github.com>
2022-07-22 17:13:10 -0500

    79 company info page jasmine (#105) (#108)
    * unfinished base for company info page
    
    * placeholders
    
    * added company info routes
    
    * added form values based on company model names
    
    * fixed edit info bug
    
    * small style comment
    
    * added table variables
    
    * retrieve information from form to routes.py
    
    * restructure company page template
    
    * removed company info routes
    
    * made company info routes file based on blogposts
    
    * added create company feature
    
    * adjustments to inputs
    
    * added asterisk to required fields
    
    * took out values
    
    * create company button
    
    * Update company_info.py
    
    * Update company_info.html
    
    * PR changes (states, profile routes)
    
    * company membership table
    
    added the company id and current_user.id to company membership table when creating a company
    
    * update edit company info
    
    * update edit company info
    
    * added updates to company_info.py to profiles.py
    
    Co-authored-by: Stuart Anderson <58710710+stuart909@users.noreply.github.com>
    Co-authored-by: kartikgrover <80264379+kartikgrover@users.noreply.github.com>
    
    Co-authored-by: Stuart Anderson <58710710+stuart909@users.noreply.github.com>
    Co-authored-by: kartikgrover <80264379+kartikgrover@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from .models import User
+from .models import User, Company
 
 main = Blueprint('main', __name__)
 
@@ -58,72 +58,3 @@ def products():
 @main.route("/forums")
 def forums():
     return render_template('forums.html', title ='forums', posts = posts)
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
-
-
-#edit profile info post
-@main.route('/profile/edit_profile', methods=['POST'])
-@login_required
-def edit_profile_post():
-    old_email = request.form.get('old_email')
-    name = request.form.get('name')
-    email = request.form.get('email')
-    # Find user
-    user = User.query.filter_by(email=old_email).first()
-
-    # Update name
-    user.name = name
-    user.email = email
-    db.session.commit()
-    
-    # Reload profile page
-    return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
-
-#edit password post
-#TODO: Password validation and requirements and check for empty inputs
-
-@main.route('/profile/edit_password', methods=['POST'])
-@login_required
-def edit_password_post():
-    email = request.form.get('email')
-    curr_password = request.form.get('curr_password')
-    new_password = request.form.get('new_password')
-    confirm_new_password = request.form.get('confirm_new_password')
-    
-    # Find user
-    user = User.query.filter_by(email=email).first()
-
-    # Check if current password is correct
-    def check_curr_password(func):
-        def wrapper():
-            if not check_password_hash(user.password, curr_password):
-                # Flash error
-                flash('curr_password_error')
-                return False
-            return func()
-        return wrapper
-
-    # Check if new password and confirmation new password are the same
-    @check_curr_password
-    def check_confirm_new_password():
-        if new_password == confirm_new_password:
-            return True
-        else:
-            # Flash error
-            flash('new_password_confirm_error')
-            return False
-
-    # Hash and save new password
-    def set_new_password():
-        user.password = generate_password_hash(new_password, method='sha256')
-        db.session.commit()
-    
-    if check_confirm_new_password():
-        set_new_password()
-        
-    return redirect(url_for('main.profile')) # reload the page
-    
