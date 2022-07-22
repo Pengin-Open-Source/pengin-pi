c3fa2e8bc256f6211c69cd2459e6b685c8af035e
jasmineman <jmm28@rice.edu>
2022-07-22 16:39:13 -0500

    update edit company info

---
+++ app/profiles.py
@@ -0,0 +1,127 @@
+from flask import Blueprint, render_template, redirect, flash, url_for, request, abort
+from flask_login import login_required, current_user
+from . import db
+from .models import Company
+from datetime import datetime
+from flask_principal import Permission, RoleNeed
+
+
+## user profile routes
+
+profiles = Blueprint('profiles', __name__, url_prefix="/profile")
+
+@profiles.route('/profile')
+@login_required
+def profile():
+    return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
+
+
+#edit profile info post
+@profiles.route('/profile/edit_profile', methods=['POST'])
+@login_required
+def edit_profile_post():
+    old_email = request.form.get('old_email')
+    name = request.form.get('name')
+    email = request.form.get('email')
+    # find user
+    user = User.query.filter_by(email=old_email).first()
+
+    # update name
+    user.name = name
+    user.email = email
+    db.session.commit()
+    
+    # reload profile page
+    return redirect(url_for('main.profile'))
+
+@profiles.route('/edit_password')
+def edit_password():
+    return render_template('edit_password.html')
+
+@profiles.route('/edit_password', methods=['POST'])
+def edit_password_post():
+    return redirect(url_for('main.profile'))
+    
+
+
+
+## user company routes
+
+company_info = Blueprint('company_info', __name__, url_prefix="/companies")
+
+def get_companies():
+    return Company.query.all()
+
+
+@company_info.route("/")
+def display_companies_home():
+    return render_template('company_info/company_info_main.html', companies=get_companies())
+
+
+# company info
+@company_info.route('/<int:company_id>')
+@login_required
+def display_company_info(company_id):
+    company = Company.query.get_or_404(company_id)
+    return render_template('company_info/company_info.html', company=company)
+
+
+@company_info.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_company():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zipcode = request.form.get('zipcode')
+        country = request.form.get('country')
+        phone = request.form.get('phone')
+        email = request.form.get('email')
+        ## TODO members?
+
+        new_company = Company(name=name, address1=address1, address2=address2, city=city, state=state, zipcode=zipcode, country=country, phone=phone, email=email)
+        db.session.add(new_company)
+        db.session.commit()
+        return redirect(url_for("company_info.display_company", company_id=new_company.id))
+    # handle GET method
+    return render_template('company_info/company_info_create.html', companies=get_companies())
+
+
+@company_info.route('/edit_company_info/<int:company_id>', methods=['POST'])
+@login_required
+def edit_company_info_post():
+
+    # Get new information
+
+    company_id = request.form.get('id')
+    name = request.form.get('name')
+    address1 = request.form.get('address1')
+    address2 = request.form.get('address2')
+    city = request.form.get('city')
+    state = request.form.get('state')
+    zipcode = request.form.get('zipcode')
+    country = request.form.get('country')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+    members = request.form.get('members')
+
+    # find company
+    company = Company.query.filter_by(id=id).first()
+
+    # update information
+    company.name = request.form.get('name')
+    company.address1 = request.form.get('address1')
+    company.address2 = request.form.get('address2')
+    company.city = request.form.get('city')
+    company.state = request.form.get('state')
+    company.zipcode = request.form.get('zipcode')
+    company.country = request.form.get('country')
+    company.phone = request.form.get('phone')
+    company.email = request.form.get('email')
+    
+    db.session.commit()
+
+    return redirect(url_for('company_info.display_company_info', company_id=company.id))
+
