fb7cfe9ef5ef8ef5dd65bb052336aac20b58656c
kartikgrover <groverkartik2000@yahoo.com>
2022-08-01 18:15:03 +0530

    customer-system-models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from sqlalchemy import func
+from sympy import product
 from . import db
 
 
@@ -59,3 +60,69 @@ class CompanyMembers(db.Model):
     company_id = db.Column(db.Integer(), db.ForeignKey('company.id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
+class Forum_Post(db.Model):
+     __tablename__ = "forum_post"
+     id = db.Column(db.Integer(), primary_key=True)
+     title = db.Column(db.String(), unique=True)
+     content = db.Column(db.String())
+     thread = db.Column(db.String())
+     author = db.Column(db.String())
+     tags = db.Column(db.String(), unique=True)
+     date = db.Column(db.Integer(), unique=True)
+
+
+
+class Customer(db.Model):
+    __tablename__ = "customer"
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(),db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    company_id = db.Column(db.Integer(), db.ForeignKey('company.id', ondelete='CASCADE'), nullable=False)
+    order_id= db.column(db.Integer())
+    order_id = db.Column(db.Integer(), db.ForeignKey('order.id', ondelete='CASCADE')) 
+    date = db.Column(db.DateTime(timezone=True),server_default=func.now())
+    service_date = db.Column(db.DateTime(255), nullable=True) 
+    expiration_date = db.Column(db.DateTime(255), nullable=True)
+
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer(), primary_key=True)
+    #product_id = 
+    order_date = db.Column(db.DateTime(timezone=True),server_default=func.now())
+    expected_service_date = db.Column(db.DateTime(255), nullable=True) 
+    expiration_date = db.Column(db.DateTime(255), nullable=True)
+
+class Contracts(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    customer_id = db.Column(db.Integer(),db.ForeignKey('customer.id', ondelete='CASCADE'), nullable=False)
+    #contract_type = 
+    expiration_date = db.Column(db.DateTime(255), nullable=True)
+
+
+
+
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    customer_id = db.Column(db.Integer(),db.ForeignKey('customer.id', ondelete='CASCADE'), nullable=False)
+    forum_post_id = db.Column(db.Integer(),db.ForeignKey('forum_post.id', ondelete='CASCADE'), nullable=False)
+    #thread_id
+
+
+
+
+class ShippingAddress(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    address1 = db.Column(db.String())
+    address2 = db.Column(db.String())
+    city = db.Column(db.String())
+    state = db.Column(db.String())
+    country = db.Column(db.String())
+    zipcode = db.Column(db.String())
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    description = db.Column(db.String(10000))
+    price = db.Column(db.String())
