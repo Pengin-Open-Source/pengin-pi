3a287980b173d029b5fa6b440f419b9c9265051b
achen236 <57299247+achen236@users.noreply.github.com>
2022-08-18 15:03:30 -0400

    edit ticket post
    login now required for ticket routes

--- app/tickets.py
+++ app/tickets.py
@@ -6,23 +6,27 @@ from .models import Ticket, TicketForum, Customer
 tickets = Blueprint('tickets', __name__)
 
 @tickets.route("/tickets")
+@login_required
 def tickets_view():
     tickets = TicketForum.query.order_by(TicketForum.date.desc()).all()
     return render_template('ticket/ticket_main.html', tickets=tickets)
 
 # Create ticket view
 @tickets.route("/tickets/create_ticket", methods=['GET'])
+@login_required
 def create_ticket_view():
     return render_template('ticket/ticket_create.html')
 
 # POST create ticket
 @tickets.route("/tickets/create_ticket", methods=['POST'])
+@login_required
 def create_ticket():
     # Check if user is a customer
     customer = Customer.query.filter_by(user_id=current_user.id).first()
     if not customer:
         flash("Sorry, you're currently not a customer and only customers can submit a ticket.")
-        return redirect(url_for('tickets.tickets_view')) # if not customer return to tickets
+        return redirect(url_for('main.home')) # if not customer return to home
+
     # Get form data
     summary = request.form.get('summary')
     content = request.form.get('content')
@@ -49,8 +53,48 @@ def create_ticket():
 
 # Edit ticket view
 @tickets.route("/tickets/<int:ticket>/edit_ticket", methods=['GET'])
+@login_required
 def edit_ticket_view(ticket):
-    # temp ticket var
-    ticket = {'title':"HELLO", 'content':'WORLD'}
-    
     return render_template('ticket/ticket_edit.html', ticket=ticket)
+
+@tickets.route("/tickets/<int:ticket>/edit_ticket", methods=['POST'])
+@login_required
+def edit_ticket(ticket):
+    # Check user is customer
+    customer = Customer.query.filter_by(user_id=current_user.id).first()
+    if not customer:
+        flash("Sorry, you're currently not a customer and only customers can edit a ticket.")
+        return redirect(url_for('main.home')) # if not customer return to home
+    
+    # Get ticket to be edited from database
+    ticketObj = Ticket.query.filter_by(id=ticket).first()
+
+    # Check ticket exists
+    if not ticketObj:
+        flash("Sorry, this ticket doesn't exist")
+        return redirect(url_for('tickets.tickets_view')) # if ticket doesn't exist return to tickets
+    
+    # Check if user's customer id matches ticket's customer id
+    if customer.id != ticket.customer_id:
+        flash("Sorry, you're not the author of this ticket.")
+        return redirect(url_for('tickets.tickets_view')) # if customer ids don't match return to tickets
+
+    # Get form data
+    summary = request.form.get('summary')
+    content = request.form.get('content')
+    tags = request.form.get('tags')
+
+    # Get ticket forum post
+    ticket_forum = TicketForum.query.filter_by(ticketObj.forum_post_id).first()
+    
+    if not ticket_forum:
+        flash("Sorry, this ticket doesn't have a forum post.")
+        return redirect(url_for('tickets.tickets_view')) # if ticket forum doesn't exist
+
+    ticket_forum.summary = summary
+    ticket_forum.content = content
+    ticket_forum.tags = tags
+
+    db.session.commit()
+
+    return redirect(url_for('tickets.tickets_view'))
