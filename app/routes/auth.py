338a67356888f4fe53e1075adf4dd44522d01f25
James Spencer <jameskcs1254@gmail.com>
2022-11-04 10:20:17 +0000

    move routes into folder

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
 from flask_principal import identity_changed, Identity, AnonymousIdentity
-from .models import User
-from . import db
+from ..models import User
+from .. import db
 
 auth = Blueprint('auth', __name__)
 
