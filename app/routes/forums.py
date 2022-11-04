15d5432849005326ac69c6230eb877f8753ef678
James Spencer <jameskcs1254@gmail.com>
2022-11-04 14:06:05 +0000

    fix imports

--- app/routes/forums.py
+++ app/routes/forums.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
-from ..models import User, Company, CompanyMembers, db
+from ..models import User, ForumPost, ForumComment, db
 
 #dummy variable list of dictionaries remove once DB queries setup
 posts = [
