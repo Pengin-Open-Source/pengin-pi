c5b34dffa20138794203d9c10b36f62201ff8329
James Spencer <jameskcs1254@gmail.com>
2022-11-04 11:40:19 +0000

    fix import

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,8 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
 from flask_principal import identity_changed, Identity, AnonymousIdentity
-from ..models import User
-from ..models import db
+from ..models import User, db
 
 auth = Blueprint('auth', __name__)
 
