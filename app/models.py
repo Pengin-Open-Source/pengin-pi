a9702ebbb11da777d32bf9321904b340708c2224
James Spencer <jameskcs1254@gmail.com>
2022-11-04 11:41:54 +0000

    remove comments

--- app/models.py
+++ app/models.py
@@ -6,21 +6,19 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True) 
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     roles = db.relationship('Role', secondary='user_roles')
 
 
-# Define the Role data-model
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
 
 
-# Define the UserRoles association table
 class UserRoles(db.Model):
     __tablename__ = 'user_roles'
     id = db.Column(db.Integer(), primary_key=True)
@@ -28,7 +26,6 @@ class UserRoles(db.Model):
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
 
-# copy/paste job from tobuwebflask per issue #39
 class BlogPost(db.Model):
     __tablename__ = "blogpost"
     id = db.Column(db.Integer, primary_key=True)
