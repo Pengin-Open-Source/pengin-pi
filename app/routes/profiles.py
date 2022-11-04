5809f238d7cfe7e47818cbf4b1bee86a273c38b7
James Spencer <jameskcs1254@gmail.com>
2022-11-04 11:10:27 +0000

    edit formatting

--- app/routes/profiles.py
+++ app/routes/profiles.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
-from .. import db
-from ..models import User, Company, CompanyMembers
-
-## user profile routes
+from ..models import User, Company, CompanyMembers, db
 
 profiles = Blueprint('profiles', __name__, url_prefix="/profile")
 
@@ -12,23 +9,18 @@ profiles = Blueprint('profiles', __name__, url_prefix="/profile")
 def profile():
     return render_template('profile/profile.html', name=current_user.name, email=current_user.email)
 
-
-#edit profile info post
 @profiles.route('/edit_profile', methods=['POST'])
 @login_required
 def edit_profile_post():
     old_email = request.form.get('old_email')
     name = request.form.get('name')
     email = request.form.get('email')
-    # find user
     user = User.query.filter_by(email=old_email).first()
 
-    # update name
     user.name = name
     user.email = email
     db.session.commit()
-    
-    # reload profile page
+
     return redirect(url_for('profiles.profile'))
 
 @profiles.route('/edit_password')
@@ -40,10 +32,6 @@ def edit_password_post():
     return redirect(url_for('profiles.profile'))
     
 
-
-
-## user company routes
-
 company_info = Blueprint('company_info', __name__, url_prefix="/companies")
 
 def get_companies():
@@ -54,14 +42,11 @@ def get_companies():
 def display_companies_home():
     return render_template('company_info/company_info_main.html', companies=get_companies())
 
-
-# company info
 @company_info.route('/<int:company_id>')
 @login_required
 def display_company_info(company_id):
     company = Company.query.get_or_404(company_id)
     return render_template('company_info/company_info.html', company=company)
-
 
 @company_info.route('/create', methods=['GET', 'POST'])
 @login_required
@@ -78,25 +63,20 @@ def create_company():
         email = request.form.get('email')
 
         new_company = Company(name=name, address1=address1, address2=address2, city=city, state=state, zipcode=zipcode, country=country, phone=phone, email=email)
-        #add to the company membership table
         db.session.add(new_company)
         db.session.commit()
         new_members_company = CompanyMembers(id = new_company.id , user_id = current_user.id)
         db.session.add(new_members_company)
         db.session.commit()
-        return redirect(url_for("company_info.display_company_info", company_id=new_company.id))
-    # handle GET method
-    return render_template('company_info/company_info_create.html', companies=get_companies())
 
+        return redirect(url_for("company_info.display_company_info", company_id=new_company.id))
+
+    return render_template('company_info/company_info_create.html', companies=get_companies())
 
 @company_info.route('/edit_company_info/<int:company_id>', methods=['POST'])
 @login_required
 def edit_company_info_post(company_id):
-
-    # find company
     company = Company.query.filter_by(id=company_id).first()
-
-    # update information
     company.name = request.form.get('name')
     company.address1 = request.form.get('address1')
     company.address2 = request.form.get('address2')
@@ -106,7 +86,6 @@ def edit_company_info_post(company_id):
     company.country = request.form.get('country')
     company.phone = request.form.get('phone')
     company.email = request.form.get('email')
-    
     db.session.commit()
 
     return redirect(url_for('company_info.display_company_info', company_id=company.id))
