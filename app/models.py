8549de7ab0e59bba9057d34e2feec1ca862bb507
James Spencer <jameskcs1254@gmail.com>
2022-11-04 13:52:10 +0000

    Add forum post and forum comment models

--- app/models.py
+++ app/models.py
@@ -57,3 +57,22 @@ class CompanyMembers(db.Model):
     company_id = db.Column(db.Integer(), db.ForeignKey('company.id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
+class ForumPost(db.Model):
+    __tablename__ = "forum_post"
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), unique=True)
+    content = db.Column(db.String())
+    thread = db.Column(db.String())
+    author = db.Column(db.String())
+    tags = db.Column(db.String(), unique=True)
+    date = db.Column(db.Integer(), unique=True)
+    comment = db.relationship('ForumComment')
+
+
+class ForumComment(db.Model):
+    __tablename__ = "forum_comment"
+    id = db.Column(db.Integer(), primary_key=True)
+    post_id = db.Column(db.Integer(), db.ForeignKey('forum_post.id', ondelete='CASCADE'))
+    content = db.Column(db.String())
+    author = db.Column(db.String())
+    date = db.Column(db.Integer(), unique=True)
