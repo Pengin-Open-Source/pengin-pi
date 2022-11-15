b5eec667d41a71803ccbb84c335e37ad048eb741
James Spencer <jameskcs1254@gmail.com>
2022-11-15 17:56:29 +0000

    Import admin

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask_principal import Permission, RoleNeed
-from . import admin
+from flask_admin import Admin
 from . import models
 from . import db
 
