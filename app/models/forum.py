6148be1549fd0026c9a6ae433bcd499ee5dbc37f
James Spencer <jameskcs1254@gmail.com>
2022-11-15 18:55:42 +0000

    fix forum_post model

--- app/models/forum.py
+++ app/models/forum.py
@@ -1,16 +1,17 @@
 from .db import db
 from sqlalchemy import func
 
+
 class ForumPost(db.Model):
     __tablename__ = "forum_post"
     id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(), unique=True)
+    title = db.Column(db.String())
     content = db.Column(db.String())
-    thread = db.Column(db.String())
+    thread = db.Column(db.Integer(), db.ForeignKey('thread.id', ondelete='CASCADE'))
     author = db.Column(db.String())
-    tags = db.Column(db.String(), unique=True)
-    date = db.Column(db.Integer(), unique=True)
-    comment = db.relationship('ForumComment')
+    tags = db.Column(db.String())
+    date = db.Column(db.Integer())
+    comments = db.relationship('ForumComment')
 
 
 class ForumComment(db.Model):
@@ -20,27 +21,16 @@ class ForumComment(db.Model):
         'forum_post.id', ondelete='CASCADE'))
     content = db.Column(db.String())
     author = db.Column(db.String())
-    date = db.Column(db.Integer(), unique=True)
+    date = db.Column(db.Integer())
     zipcode = db.Column(db.Integer())
 
-#adding forum_post from #112
-class Forum_Post(db.Model):
-    __tablename__ = 'forum_post'
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String())
-    content = db.Column(db.String())
-    thread = db.Column(db.String())
-    author = db.Column(db.String())
-    tags = db.Column(db.String())
-    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
-   # comments = db.relationship('Forum_Comment', secondary='ticket_forum')
-       
 
 class Thread(db.Model):
     __tablename__ = "thread"
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String())
     roles = db.relationship('Role', secondary='thread_roles')
+
 
 class ThreadRoles(db.Model):
     __tablename__ = "thread_roles"
