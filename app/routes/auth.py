9aa73272594fea954b9f0d266572a892c9eb3798
James Spencer <jameskcs1254@gmail.com>
2022-11-16 12:03:43 +0000

    fix function call

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -42,7 +42,7 @@ def signup_post():
 
         return redirect(url_for('auth.signup'))
 
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = model.User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
     model.db.session.add(new_user)
     model.db.session.commit()
 
