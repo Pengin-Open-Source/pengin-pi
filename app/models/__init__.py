75ec6285cb91c8f7067a580290d40149bd989918
James Spencer <jameskcs1254@gmail.com>
2022-11-16 11:28:14 +0000

    create API for models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,11 @@
-
 # TODO Should we import all models into __init__.py for simplicity of importing models in the rest of the app?
 
+from .user import User, UserRoles, Role
+from .config import db
+from .blog import BlogPost
+from .company import Company, CompanyMembers
+from .contracts import  Contracts
+from .forum import ForumComment, ForumPost, Thread, ThreadRoles
+from .orders import Order, ShippingAddress, Customer
+from .product import Product
+from .ticket import Ticket, TicketComment, TicketForum, Resolution
