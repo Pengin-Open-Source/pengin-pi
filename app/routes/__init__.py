6c8201b305f290760cf0317acac6cb45d24639cd
James Spencer <jameskcs1254@gmail.com>
2022-11-16 11:29:04 +0000

    Create API

---
+++ app/routes/__init__.py
@@ -0,0 +1,6 @@
+from .forums import forums_blueprint
+from .auth import auth as auth_blueprint
+from .blogPosts import blogPosts as blogPosts_blueprint
+from .main import main as main_blueprint
+from .profiles import profiles as profile_blueprint
+from .profiles import company_info as company_blueprint
