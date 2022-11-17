7f08b34c8a13a85df40fd2af008689af3b736296
James Spencer <jameskcs1254@gmail.com>
2022-11-17 15:06:34 +0000

    add create thread route

--- app/routes/forums.py
+++ app/routes/forums.py
@@ -3,20 +3,42 @@ from flask_login import login_required, current_user
 from flask_principal import Permission, RoleNeed
 from app.db.models import Thread, ForumPost, ForumComment
 from app.admin import admin_permission
+from app.db import db
 
 
 forums_blueprint = Blueprint('forums_blueprint', __name__, url_prefix="/forums")
 
 @forums_blueprint.route("/")
-@admin_permission.require()
 def forums():
   threads = Thread.query.filter_by().all()
   return render_template('forums/threads.html', title ='Forum', threads = threads)
+
+@forums_blueprint.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_thread():
+    if request.method == 'POST':
+        thread = request.form.get('thread')
+        new_thread = Thread(name=thread)
+        db.session.add(new_thread)
+        db.session.commit()
+        return redirect(url_for("forums_blueprint.forums"))
+    return render_template('forums/create_thread.html')
 
 @forums_blueprint.route("/<thread>")
 def thread(thread):
   posts = ForumPost.query.filter_by(thread=thread).all()  
   return render_template('forums/thread.html', thread = thread, title = thread, posts = posts)
+
+@forums_blueprint.route('/<thread>/create', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == 'POST':
+        thread = request.form.get('thread')
+        new_thread = Thread(name=thread)
+        db.session.add(new_thread)
+        db.session.commit()
+        return redirect(url_for("forums_blueprint.forums"))
+    return render_template('forums/create_thread.html')
 
 @forums_blueprint.route("/<thread>/<post_title>")
 def post(post_title, thread):
