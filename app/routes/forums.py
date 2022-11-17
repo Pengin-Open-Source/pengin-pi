7d4f959b7540d7b63a971e566dbd9d5b94bacc26
James Spencer <jameskcs1254@gmail.com>
2022-11-17 16:11:40 +0000

    add comment post to route

--- app/routes/forums.py
+++ app/routes/forums.py
@@ -11,6 +11,7 @@ forums_blueprint = Blueprint('forums_blueprint', __name__, url_prefix="/forums")
 @forums_blueprint.route("/")
 def forums():
   threads = Thread.query.filter_by().all()
+
   return render_template('forums/threads.html', title ='Forum', threads = threads)
 
 @forums_blueprint.route('/create', methods=['GET', 'POST'])
@@ -21,12 +22,15 @@ def create_thread():
     new_thread = Thread(name=thread)
     db.session.add(new_thread)
     db.session.commit()
+
     return redirect(url_for("forums_blueprint.forums"))
+
   return render_template('forums/create_thread.html')
 
 @forums_blueprint.route("/<thread>")
 def thread(thread):
   posts = ForumPost.query.filter_by(thread=thread).all()  
+
   return render_template('forums/thread.html', thread = thread, title = thread, posts = posts)
 
 @forums_blueprint.route('/<thread>/create', methods=['GET', 'POST'])
@@ -41,11 +45,26 @@ def create_post(thread):
     new_post = ForumPost(title=title, thread=thread, content=content, tags= tags, date= today, author=author)
     db.session.add(new_post)
     db.session.commit()
-    return redirect(url_for(f"forums_blueprint.thread", thread=thread))
+
+    return redirect(url_for("forums_blueprint.thread", thread=thread))
+
   return render_template('forums/create_post.html', thread=thread)
 
-@forums_blueprint.route("/<thread>/<post_title>")
+@forums_blueprint.route("/<thread>/<post_title>", methods=['GET', 'POST'])
 def post(post_title, thread):
+  if request.method == 'POST':
+    post = ForumPost.query.filter_by(title=post_title).first()
+    post_id = post.id
+    content = request.form.get('content')
+    today = date.today()
+    author = current_user.name
+    new_comment = ForumComment( content=content, post_id= post_id, date= today, author=author)
+    db.session.add(new_comment)
+    db.session.commit()
+
+    return redirect(url_for("forums_blueprint.post", post_title=post_title, thread=thread))
+
   post = ForumPost.query.filter_by(title=post_title).first()
   comments = ForumComment.query.filter_by(post_id=post.id).all()
-  return render_template('forums/post.html', title = post_title, post = post, comments = comments)
+
+  return render_template('forums/post.html', title = post_title, post = post, comments = comments, thread=thread)
