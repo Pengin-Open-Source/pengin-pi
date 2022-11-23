27719ac45de055103ffa71227d9d173eb442cf32
James Spencer <jameskcs1254@gmail.com>
2022-11-23 16:30:59 +0000

    register ticket routes

--- app/__init__.py
+++ app/__init__.py
@@ -49,5 +49,6 @@ def create_app():
     app.register_blueprint(route.profile_blueprint)
     app.register_blueprint(route.company_blueprint)
     app.register_blueprint(route.forums_blueprint)
+    app.register_blueprint(route.ticket_blueprint)
 
     return app
