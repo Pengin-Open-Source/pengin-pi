adf8803fc5332147e4e4519694116b7c3c25805a
James Spencer <jameskcs1254@gmail.com>
2022-11-23 16:31:10 +0000

    add ticket blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,3 +4,4 @@ from app.routes.forums import forums_blueprint
 from app.routes.blogPosts import blogPosts as blogPosts_blueprint
 from app.routes.profiles import profiles as profile_blueprint
 from app.routes.profiles import company_info as company_blueprint
+from app.routes.tickets import ticket_blueprint
