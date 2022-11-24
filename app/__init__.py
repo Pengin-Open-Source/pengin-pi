588e415321aecf9929982b003a03163095ed51d5
stuart <stuart@tobupengin.com>
2022-11-24 22:26:37 +0900

    some tweaks, almost fixed db

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from flask_login import LoginManager, current_user
 from flask_principal import Principal, UserNeed, RoleNeed, identity_loaded, AnonymousIdentity
 from app.admin import admin_blueprint, admin
 import app.routes as route
-import app.db.models as model
+import app.db.models as models
+from app.db import db
+
 
 
 principals = Principal()
@@ -13,18 +15,24 @@ def create_app():
     app = Flask(__name__, static_folder='static')
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = { "pool_pre_ping": True,
+    "pool_recycle": 300,}
+    app.config["SQLALCHEMY_BINDS"] = {'db': 'sqlite:///db.sqlite'}
     # adding to suppress warning, will delete later
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    model.db.init_app(app)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    
+    db.init_app(app)
     login_manager.init_app(app) 
     principals.init_app(app) 
     admin.init_app(app)
-    login_manager.login_view = 'auth.login' 
+    login_manager.login_view = 'auth.login'
+    
+    
 
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
-        return model.User.query.get(user_id)
+        return models.User.query.get(user_id)
 
     @identity_loaded.connect_via(app)
     def on_identity_loaded(sender, identity):
