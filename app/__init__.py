f983761b014567dd4b29db155796558ed9903246
stuart <stuart@tobupengin.com>
2022-12-07 17:50:03 +0900

    fixed robots.txt

--- app/__init__.py
+++ app/__init__.py
@@ -47,7 +47,6 @@ def create_app():
 
     @app.route('/robots.txt')
     @app.route('/sitemap.xml')
-
     def static_from_root():
         return send_from_directory(app.static_folder, request.path[1:])
 
