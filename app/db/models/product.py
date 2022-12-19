084c7f6bddc1109a4b8880545dd47a52dfb0fe17
James Spencer <jameskcs1254@gmail.com>
2022-12-19 12:57:05 +0000

    add columns to model

--- app/db/models/product.py
+++ app/db/models/product.py
@@ -8,3 +8,6 @@ class Product(db.Model):
     name = db.Column(db.String())
     description = db.Column(db.String())
     price = db.Column(db.String())
+    article = db.Column(db.String())
+    card_image_id = db.Column(db.String())
+    stock_image_id = db.Column(db.String())
