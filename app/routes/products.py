fcb87d92b120fb098c7aad59d8ece2a49d737457
James Spencer <jameskcs1254@gmail.com>
2022-12-19 15:26:09 +0000

    create product view basic routes

--- app/routes/products.py
+++ app/routes/products.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.util.security import admin_permission, user_permission
 from app.db import db
-from app.db.models import User
+from app.db.models import Product
 
 product_blueprint = Blueprint('product_blueprint',
                               __name__, url_prefix="/products")
@@ -11,17 +11,19 @@ product_blueprint = Blueprint('product_blueprint',
 
 @product_blueprint.route('/')
 def products():
+    products = Product.query.filter_by().all()
 
-    return render_template('products/products.html')
+    return render_template('products/products.html', products=products)
 
 
 @product_blueprint.route('/<product_id>')
 def product(product_id):
+    product = Product.query.filter_by(id=product_id).first()
 
-    return render_template('products/product.html', product_id=product_id)
+    return render_template('products/product.html', product=product)
 
 
-@product_blueprint.route('/create_product', methods=['GET', 'POST'])
+@product_blueprint.route('/create', methods=['GET', 'POST'])
 @login_required
 @admin_permission.require()
 def create_product():
@@ -31,7 +33,7 @@ def create_product():
     return render_template('products/product_create.html')
 
 
-@product_blueprint.route('/edit_product', methods=['GET', 'POST'])
+@product_blueprint.route('/edit', methods=['GET', 'POST'])
 @login_required
 @admin_permission.require()
 def edit_product():
