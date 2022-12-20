bdb3a4859103bb07368cc0e0ace9de68479c6d1d
James Spencer <jameskcs1254@gmail.com>
2022-12-20 17:27:58 +0000

    Add GET method and template render

--- app/routes/file_upload.py
+++ app/routes/file_upload.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, request
+from flask import Blueprint, redirect, request, render_template
 from app.util.s3 import upload_file_to_s3
 from werkzeug.utils import secure_filename
 from app import create_app
@@ -9,20 +9,23 @@ file_upload_blueprint = Blueprint('file_upload_blueprint',
                                   __name__, url_prefix="/upload")
 
 
-@file_upload_blueprint.route("/", methods=["POST"])
+@file_upload_blueprint.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if "file" not in request.files:
-        return "No file key in request.files"
+    if request.method == 'POST':
+        if "file" not in request.files:
+            return "No file key in request.files"
 
-    file = request.files["file"]
+        file = request.files["file"]
 
-    if file.filename == "":
-        return "Please select a file"
+        if file.filename == "":
+            return "Please select a file"
 
-    if file:
-        file.filename = secure_filename(file.filename)
-        output = upload_file_to_s3(file, app.config["S3_BUCKET"])
-        return str(output)
+        if file:
+            file.filename = secure_filename(file.filename)
+            output = upload_file_to_s3(file, app.config["S3_BUCKET"])
+            return str(output)
 
-    else:
-        return redirect("/")
+        else:
+            return redirect("/")
+
+    return render_template('products/product_image_upload.html')
