4086d1481970b8ad3a0aa8b0936c723f5dab138b
James Spencer <jameskcs1254@gmail.com>
2022-12-21 14:08:35 +0000

    add image upload logic

--- app/routes/products.py
+++ app/routes/products.py
@@ -33,9 +33,38 @@ def create_product():
         name = request.form.get('name')
         price = request.form.get('price')
         description = request.form.get('description')
-        url = '/static/images/test.png'
+
+        # Image upload handling
+        if "file-large" not in request.files:
+            large_url = '/static/images/test.png'
+
+        large_file = request.files["file-large"]
+
+        if large_file.filename == "":
+            large_url = '/static/images/test.png'
+
+        if large_file:
+            large_file.filename = secure_filename(large_file.filename)
+            large_output = upload_file_to_s3(large_file,
+                                             os.getenv("S3_BUCKET"))
+            large_url = large_output
+
+        if "file-small" not in request.files:
+            small_url = '/static/images/test.png'
+
+        small_file = request.files["file-small"]
+
+        if small_file.filename == "":
+            small_url = '/static/images/test.png'
+
+        if small_file:
+            small_file.filename = secure_filename(small_file.filename)
+            small_output = upload_file_to_s3(large_file,
+                                             os.getenv("S3_BUCKET"))
+            small_url = small_output
+
         product = Product(name=name, price=price, description=description,
-                          card_image_url=url, stock_image_url=url)
+                          card_image_url=small_url, stock_image_url=large_url)
 
         db.session.add(product)
         db.session.commit()
@@ -105,63 +134,3 @@ def upload_file(id):
                                     product_id=id))
 
     return render_template('products/product_image_upload.html')
-
-
-@product_blueprint.route('/upload-large/<id>', methods=['POST'])
-def upload_file_large(id):
-    if "file" not in request.files:
-
-        return "No file key in request.files"
-
-    file = request.files["file"]
-
-    if file.filename == "":
-
-        return "Please select a file"
-
-    if file:
-        file.filename = secure_filename(file.filename)
-        output = upload_file_to_s3(file, os.getenv("S3_BUCKET"))
-
-        product = Product.query.filter_by(id=id).first()
-        product.card_image_url = output
-        product.stock_image_url = output
-
-        db.session.commit()
-
-        return redirect(request.url)
-
-    else:
-
-        return redirect(url_for('product_blueprint.product',
-                                product_id=id))
-
-
-@product_blueprint.route('/upload-small/<id>', methods=['POST'])
-def upload_file_small(id):
-    if "file" not in request.files:
-
-        return "No file key in request.files"
-
-    file = request.files["file"]
-
-    if file.filename == "":
-
-        return "Please select a file"
-
-    if file:
-        file.filename = secure_filename(file.filename)
-        output = upload_file_to_s3(file, os.getenv("S3_BUCKET"))
-
-        product = Product.query.filter_by(id=id).first()
-        product.card_image_url = output
-        product.stock_image_url = output
-
-        db.session.commit()
-
-        return redirect(request.url)
-
-    else:
-
-        return redirect(url_for('product_blueprint.product',
-                                product_id=id))
