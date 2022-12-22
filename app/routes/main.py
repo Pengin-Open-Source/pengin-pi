f2095e629f299b5426b73a2602e7b8138d95c5b1
James Spencer <jameskcs1254@gmail.com>
2022-12-22 12:18:02 +0000

    remove unused routes

--- app/routes/main.py
+++ app/routes/main.py
@@ -8,13 +8,3 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     return render_template('home/home.html')
-
-
-@main.route("/about")
-def about():
-    return render_template('about/about.html', title='about')
-
-
-@main.route("/products")
-def products():
-    return render_template('products/products.html', title='products')
