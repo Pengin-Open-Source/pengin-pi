52905f5395e0d832d85da9e814f5b0ddc169f4a9
James Spencer <jameskcs1254@gmail.com>
2022-12-22 14:32:04 +0000

    add model update logic

--- app/routes/about.py
+++ app/routes/about.py
@@ -16,21 +16,35 @@ def view():
                            current_user=current_user)
 
 
-@about_blueprint.route('/about/edit', methods=['GET', 'POST'])
+@about_blueprint.route('/edit', methods=['GET', 'POST'])
 @login_required
 @admin_permission.require()
 def edit_about():
-    exists = About.query().first() is not None
+    exists = About.query.first() is not None
 
     if exists:
+        about = About.query.first()
         if request.method == 'POST':
+            about.name = request.form.get('name')
+            about.article = request.form.get('article')
+            about.facebook = request.form.get('facebook')
+            about.instagram = request.form.get('instagram')
+            about.whatsapp = request.form.get('whatsapp')
+            about.linkedin = request.form.get('linkedin')
+            about.youtube = request.form.get('youtube')
+            about.twitter = request.form.get('twitter')
+            about.phone = request.form.get('phone')
+            about.address1 = request.form.get('address1')
+            about.address2 = request.form.get('address2')
+            about.city = request.form.get('city')
+            about.state = request.form.get('state')
+            about.country = request.form.get('country')
 
-            db.session.add()
             db.session.commit()
 
             return redirect(url_for("about_blueprint.view"))
 
-        return render_template('about/edit.html')
+        return render_template('about/edit.html', about=about)
     else:
         if request.method == 'POST':
 
