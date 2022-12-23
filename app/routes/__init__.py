9f6777e2ced124d40f6458f0c209b4b870f7a8bd
James Spencer <jameskcs1254@gmail.com>
2022-12-23 11:12:14 +0000

    remove unused route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,7 +4,6 @@ from app.routes.blogPosts import blogPosts as blogPosts_blueprint
 from app.routes.calendar import calendar_blueprint
 from app.routes.companies import company_info as company_blueprint
 from app.routes.forums import forums_blueprint
-from app.routes.main import main as main_blueprint
 from app.routes.profiles import profiles as profile_blueprint
 from app.routes.tickets import ticket_blueprint
 from app.routes.calendar import calendar_blueprint
