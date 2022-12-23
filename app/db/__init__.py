6bf907b6b8273a9582423077ff731e9d0aca3e1a
stuart <stuart@tobupengin.com>
2022-12-23 20:38:42 +0900

    fixed circular import in app.db

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,2 +1,3 @@
-import app.db.models as models
 from app.db.config import db
+import app.db.models as models
+
--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,10 +1,9 @@
 from app.db import db
 from app.db.models.blog import BlogPost
-from app.db.models.calendar import Event
 from app.db.models.contracts import Contracts
-from app.db.models.customer import (Company, CompanyMembers, Customer, Order,
-                                    Role, ShippingAddress, User, UserRoles)
+from app.db.models.customer import User, UserRoles, Role, Company, CompanyMembers
 from app.db.models.forum import ForumComment, ForumPost, Thread, ThreadRoles
+from app.db.models.customer import Order, ShippingAddress, Customer
 from app.db.models.product import Product
 from app.db.models.ticket import TicketComment, TicketForum, Resolution
 from app.db.models.calendar import Event
