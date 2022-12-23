653092e269bbc50ed6163691aa5b44c5fa99ddf9
James Spencer <jameskcs1254@gmail.com>
2022-12-23 10:49:11 +0000

    fix variable

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,4 @@
-from app.routes.home import home_blueprint 
+from app.routes.home import home_blueprint
 from app.routes.auth import auth as auth_blueprint
 from app.routes.forums import forums_blueprint
 from app.routes.blogPosts import blogPosts as blogPosts_blueprint
@@ -10,4 +10,4 @@ from app.routes.calendar import calendar_blueprint
 
 # Routes variable is a list of blupeprints in this current directory
 blueprints = [val for key, val in globals().items()
-              if isinstance(val, type(main_blueprint))]
+              if isinstance(val, type(home_blueprint))]
