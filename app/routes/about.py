277c45f87ead4ca4aed6da782886706380754e5d
James Spencer <jameskcs1254@gmail.com>
2022-12-23 09:46:36 +0000

    add image logging

--- app/routes/about.py
+++ app/routes/about.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
+import logging
 from app.util.security import admin_permission
 from app.db import db
 from app.db.models import About
@@ -11,9 +12,11 @@ about_blueprint = Blueprint('about_blueprint', __name__,
 @about_blueprint.route("/")
 def view():
     about = About.query.filter_by().first()
+    about_image = About.query.filter_by().first().image
+    logging.info('Image S3 URL accessed:' + about_image)
 
     return render_template('about/about_main.html', about=about,
-                           current_user=current_user)
+                           current_user=current_user, about_image=about_image)
 
 
 @about_blueprint.route('/edit', methods=['GET', 'POST'])
@@ -23,6 +26,9 @@ def edit_about():
     # As there should only be one entry for about model it can be checked
     # whether this exists or not to allow creation or editing.
     exists = About.query.first() is not None
+
+    # TODO Add image upload handling.
+    image = 'Sort out file handling for image when products issue done'
 
     if exists:
         about = About.query.first()
@@ -41,6 +47,7 @@ def edit_about():
             about.city = request.form.get('city')
             about.state = request.form.get('state')
             about.country = request.form.get('country')
+            about.image = image
 
             db.session.commit()
 
@@ -63,13 +70,14 @@ def edit_about():
             city = request.form.get('city')
             state = request.form.get('state')
             country = request.form.get('country')
+            image = image
 
             new_about = About(name=name, article=article, facebook=facebook,
                               instagram=instagram, whatsapp=whatsapp,
                               linkedin=linkedin, youtube=youtube, phone=phone,
                               twitter=twitter, address1=address1,
                               address2=address2, city=city, state=state,
-                              country=country)
+                              country=country, image=image)
 
             db.session.add(new_about)
             db.session.commit()
