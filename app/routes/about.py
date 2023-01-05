05df849884e56b7c1669976b347064961505f9c5
Stuart Anderson <58710710+stuart909@users.noreply.github.com>
2023-01-05 12:32:15 +0900

    Closes 266 Closes 268 Fixed About and Home.  Fixed SNS links, had extra character.  Fixed current_user being sent to route.  Sending is_admin now. (#280)

--- app/routes/about.py
+++ app/routes/about.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import login_required, current_user
+from flask_login import login_required
 from app.util.security import admin_permission
 import logging
 from app.util.security import admin_permission
@@ -15,6 +15,7 @@ about_blueprint = Blueprint('about_blueprint', __name__,
 @about_blueprint.route("/")
 def view():
     about = About.query.filter_by().first()
+    is_admin = admin_permission.can()
     try:
         image = conn.get_URL(about.image)
     except:
@@ -24,7 +25,7 @@ def view():
         logging.info('Image S3 URL accessed:' + about.image)
 
     return render_template('about/about_main.html', about=about,
-                           current_user=current_user, image=image)
+                           is_admin=is_admin, image=image)
 
 
 @about_blueprint.route('/edit', methods=['GET', 'POST'])
@@ -34,12 +35,13 @@ def edit_about():
     # As there should only be one entry for about model it can be checked
     # whether this exists or not to allow creation or editing.
     exists = About.query.first() is not None
-
-    # TODO Add image upload handling which assigns S3 URl to image variable
-    image = 'Sort out file handling for image when products issue done'
-
+    
     if exists:
         about = About.query.first()
+        try:
+            image = conn.get_URL(about.image)
+        except:
+            image = "/static/images/test.png"
 
         if about:
             logging.info('Image S3 URL accessed:' + about.image)
@@ -60,49 +62,49 @@ def edit_about():
             about.state = request.form.get('state')
             about.country = request.form.get('country')
             image = request.files["file"]
-            url = image.filename if "file" in request.files and image.filename != "" else '/static/images/test.png'
-            about.image = url
-            if image:
-                image.filename = secure_filename(image.filename)
-                about.image = conn.create(image)
+            url = image.filename if "file" in request.files and image.filename != "" else about.image
+            if about.image != url:
+                about.image = url
+                if image:
+                    image.filename = secure_filename(image.filename)
+                    about.image = conn.create(image)
 
             db.session.commit()
 
             return redirect(url_for("about_blueprint.view"))
 
-        return render_template('about/edit.html', about=about)
-    else:
-        if request.method == 'POST':
-            name = request.form.get('name')
-            article = request.form.get('article')
-            facebook = request.form.get('facebook')
-            instagram = request.form.get('instagram')
-            whatsapp = request.form.get('whatsapp')
-            linkedin = request.form.get('linkedin')
-            youtube = request.form.get('youtube')
-            twitter = request.form.get('twitter')
-            phone = request.form.get('phone')
-            address1 = request.form.get('address1')
-            address2 = request.form.get('address2')
-            city = request.form.get('city')
-            state = request.form.get('state')
-            country = request.form.get('country')
-            image = request.files["file"]
-            url = image.filename if "file" in request.files and image.filename != "" else '/static/images/test.png'
-            if image:
-                image.filename = secure_filename(image.filename)
-                url = conn.create(image)
+        return render_template('about/edit.html', about=about, image=image)
+    elif request.method == 'POST':
+        name = request.form.get('name')
+        article = request.form.get('article')
+        facebook = request.form.get('facebook')
+        instagram = request.form.get('instagram')
+        whatsapp = request.form.get('whatsapp')
+        linkedin = request.form.get('linkedin')
+        youtube = request.form.get('youtube')
+        twitter = request.form.get('twitter')
+        phone = request.form.get('phone')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        country = request.form.get('country')
+        image = request.files["file"]
+        url = image.filename if "file" in request.files and image.filename != "" else '/static/images/test.png'
+        if image:
+            image.filename = secure_filename(image.filename)
+            url = conn.create(image)
 
-            new_about = About(name=name, article=article, facebook=facebook,
-                              instagram=instagram, whatsapp=whatsapp,
-                              linkedin=linkedin, youtube=youtube, phone=phone,
-                              twitter=twitter, address1=address1,
-                              address2=address2, city=city, state=state,
-                              country=country, image=url)
+        new_about = About(name=name, article=article, facebook=facebook,
+                            instagram=instagram, whatsapp=whatsapp,
+                            linkedin=linkedin, youtube=youtube, phone=phone,
+                            twitter=twitter, address1=address1,
+                            address2=address2, city=city, state=state,
+                            country=country, image=url)
 
-            db.session.add(new_about)
-            db.session.commit()
+        db.session.add(new_about)
+        db.session.commit()
 
-            return redirect(url_for("about_blueprint.view"))
+        return redirect(url_for("about_blueprint.view"))
 
-        return render_template('about/create.html')
+    return render_template('about/create.html')
