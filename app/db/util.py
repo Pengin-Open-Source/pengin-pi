138c72f04d377936d9a786525122d28f62b735ce
Stuart Anderson <58710710+stuart909@users.noreply.github.com>
2023-01-26 22:03:44 +0900

    315 paginate api (#344)
    * 315: paginate api (#343)
    
    * 315: paginate API
    
    * Update blog_posts.py
    
    * Update calendar.py
    
    * Update forums.py
    
    * Update products.py
    
    * Update tickets.py
    
    Co-authored-by: Loc Pham <loc.pham@advocado.me>
    Co-authored-by: Stuart Anderson <58710710+stuart909@users.noreply.github.com>
    
    * added some fixes, will need to do some tweaks later
    
    Co-authored-by: Andrew Pham <66355109+adp11@users.noreply.github.com>
    Co-authored-by: Loc Pham <loc.pham@advocado.me>

--- app/db/util.py
+++ app/db/util.py
@@ -1,11 +1,11 @@
 from math import ceil
-from sqlalchemy import desc, asc
+from sqlalchemy import desc
+    
 
 def paginate(Model:object, page:int, **kwargs)->list:
     """Paginate Method
     This paginate method takes a SQLAlchemy model, a page number,
-    and keywords: order, pages and key.  The output is a possible paginated
-    list of SQL data or an empty list.
+    and keywords: order, pages, key, filters. The output is a possible paginated list of SQL data or an empty list.
     
     Required Inputs:
         Model: SQLAlchemy ORM class
@@ -15,6 +15,7 @@ def paginate(Model:object, page:int, **kwargs)->list:
         order=desc or asc (descending or ascending, from sqlalchemy)
         pages=int  the per_page attribute for a query, number of items per page
         key=str   Model attribute to look for IE: 'id','name', or 'date'  
+        filters=dict    dictionary of filtering conditions
     
     Outputs:
         Your paginated SQL query data...hopefully.
@@ -23,9 +24,13 @@ def paginate(Model:object, page:int, **kwargs)->list:
         page = 1
     pages = kwargs.get('pages', 10)
     order = kwargs.get('order', desc)
+    filters = kwargs.get("filters", {})
     key = kwargs.get('key', 'id')
     try:
         query = Model.query
+        if filters:
+            query = Model.query.filter_by(**filters)
+
         if key:
             order_attr = getattr(Model, key)
             query = query.order_by(order(order_attr))
