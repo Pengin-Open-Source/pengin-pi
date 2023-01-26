95660ce39561604c4778d4aad20ac83bb5e8cc50
Andrew Pham <66355109+adp11@users.noreply.github.com>
2023-01-26 07:06:04 -0500

    315: paginate api (#343)
    * 315: paginate API
    
    * Update blog_posts.py
    
    * Update calendar.py
    
    * Update forums.py
    
    * Update products.py
    
    * Update tickets.py
    
    Co-authored-by: Loc Pham <loc.pham@advocado.me>
    Co-authored-by: Stuart Anderson <58710710+stuart909@users.noreply.github.com>

--- app/routes/blog_posts.py
+++ app/routes/blog_posts.py
@@ -4,6 +4,7 @@ from flask_principal import Permission, RoleNeed
 
 from app.db import db
 from app.db.models import BlogPost
+from app.db.util import paginate
 
 blogPosts = Blueprint('blogPosts', __name__)
 admin_permission = Permission(RoleNeed('admin'))
@@ -13,10 +14,15 @@ def get_links():
     return BlogPost.query.all()
 
 
-@blogPosts.route("/blog")
+@blogPosts.route("/blog", methods=["GET", "POST"])
 def display_blog_home():
-    posts = BlogPost.query.limit(15)
-    return render_template('blog/blog.html', posts=posts, links=get_links(),
+    if request.method == "POST":
+        page = int(request.form.get('page_number', 1))
+    else:
+        page = 1
+
+    posts = paginate(BlogPost, page=page, key="title", pages=10)
+    return render_template('blog/blog.html', posts=posts,
                            is_admin=admin_permission.can())
 
 
