1105fe67c4b0d24939e207f3dbff936621ad41bd
James Spencer <jameskcs1254@gmail.com>
2023-02-07 13:13:15 +0000

    import datetime add conditional date assignment

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -4,7 +4,7 @@ from flask_principal import Permission, RoleNeed
 from app.db import db
 from app.util.uuid import id as ID
 from app.db.models import Orders, OrdersList, Product, Customer, User
-from itertools import groupby
+import datetime
 
 
 order_info = Blueprint('order_info', __name__, url_prefix="/orders")
@@ -33,14 +33,16 @@ def display_order_info(order_id):
 @admin_permission.require()
 def create_order():
     if request.method == 'POST':
-        order_date = request.form.get('order_date') # TODO or datetime.utcnow() if null
+        order_date = request.form.get('order_date') or datetime.utcnow() 
         customer_id = request.form.get('customer_id')
         product_id = request.form.getlist('product_id')
         quantity = request.form.getlist('quantity')
         orders = [{'product': product, 'qty': qty} for product, qty in dict(zip(product_id, quantity)).items()]
         order_id = ID()
+        
         new_order = Orders(id=order_id, order_date=order_date, customer_id=customer_id)
         db.session.add(new_order)
+
         for order in orders:
             new_order_list = OrdersList(quantity=order['qty'], orders_id=order_id, product_id=order['product'])
             db.session.add(new_order_list)
