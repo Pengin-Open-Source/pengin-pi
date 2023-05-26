b01b5013e9924f609568d58633c9790086775b75
Marc Cocchio <marccocchio@gmail.com>
2023-05-26 16:27:54 +0900

    changed application route and db model

--- app/db/models/application.py
+++ app/db/models/application.py
@@ -1,13 +1,17 @@
 from app.db import db
 from app.util.uuid import id
 from job import Job
+from datetime import datetime
+
 
 class Application(db.Model): #399-application-crm
     __tablename__ = 'application'
     id = db.Column(db.String(36), default=id, primary_key=True)
     job_id = db.Column(db.String(36), db.ForeignKey(Job.id)) #matches the id of the job for which the applicant is applying
+    job_name = db.Column(db.String(100), db.ForeignKey(Job.job_title))
+    current_user =db.Column(db.String(),db.ForeignKey())
     message_to_hiring_manager = db.Column(db.Text)
-    date_applied = db.Column(db.String(100))
+    date_applied = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
     location_of_candidate = db.Column(db.String(100))
-    resume_path = x #look at our images file framework for ideas
-    cover_letter_path = y #look at our images file framework for ideas
+    resume_path = db.Column(db.String(500)) #look at our images file framework for ideas
+    cover_letter_path = db.Column(db.String(500)) #look at our images file framework for ideas
--- app/routes/applications.py
+++ app/routes/applications.py
@@ -9,24 +9,27 @@ from app.db.util import paginate
 application_blueprint = Blueprint('application_blueprint',
                                   __name__, url_prefix='/applications')
 #398-application-route
-g
+
 @application_blueprint.route('/create/<job_id>', methods=['GET','POST']) #CREATE (applicant submits their application)
 @login_required
 def create_application(job_id):
     if request.method == 'POST':
         message_to_hiring_manager = request.form.get('message_to_hiring_manager') #wait... do I need "get" here????
         location_of_candidate = request.form.get('location_of_candidate')
-        resume_path = x #look at our images file framework for ideas
-        cover_letter_path = y #look at our images file framework for ideas
-        
+        resume_path = x #how to save files to db function?(request.files.get('resume'))
+        cover_letter_path = y #how to save files to db function?(request.files.get('cover_letter'))
+                
         application = Application(message_to_hiring_manager=message_to_hiring_manager,
                                   location_of_candidate=location_of_candidate,
                                   resume_path=resume_path,
-                                  cover_letter_path=cover_letter_path)
+                                  cover_letter_path=cover_letter_path,
+                                  job_id=job_id,
+                                  user_id=current_user.id 
+                                  )#something to associate logged in user... help here?
         
         db.session.add(application)
         db.session.commit()
-        #flash('Your application has been submitted.')
+        #flash('Your application has been submitted.') or something similar
         return redirect(url_for(application_blueprint.application))
     return render_template('applications/application_create.html', primary_title='Create Application')
 
@@ -36,10 +39,13 @@ def create_application(job_id):
 @login_required
 def application(job_id):
     is_admin = admin_permission.can()
-    application = Application.b.query.filter_by(job_id=job_id).first()
+    application = Application.b.query.filter_by(job_id=job_id, user_id=current_user.id).first() #again here I need a current user to be associated?
+    
+    application.resume_path = conn.get_URL(application.resume_path)
+    application.cover_letter_path = conn.get_URL(application.cover_letter_path)
     
     return render_template('applications/application.html',is_admin=is_admin,application=application,
-                           page=1, primary_title=application.job_title + " Application") #hope this works
+                           page=1, primary_title=application.job_name + " Application") #hope this title works
 
 
 
@@ -47,7 +53,7 @@ def application(job_id):
 The actual job needs to have an apply button that uses this route.
 The page needs to load our application view for that job.
 The GET method will need to render the application form.
-NOTE: We must require someone to be authenticated.
+AND: We must require someone to be authenticated.
 The authenticated user decorator must be applied to the application route.
 Secondly, we need another route that handles the POST method
 that will take the information provided by the application form and store it in the application table.
