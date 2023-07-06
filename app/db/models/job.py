fac721f680b252560ec4c8602b1e46606ab4665a
Marc Cocchio <marccocchio@gmail.com>
2023-07-06 15:45:33 +0900

    Jobs model, routes, templates, blueprint test branch

---
+++ app/db/models/job.py
@@ -0,0 +1,15 @@
+from app.db import db
+from app.util.uuid import id
+from datetime import datetime
+
+class Job(db.Model):
+    __tablename__ = 'job'
+    id = db.Column(db.String(36), default=id, primary_key=True)
+    job_title = db.Column(db.String(100))
+    short_description = db.Column(db.String(280))
+    long_description = db.Column(db.Text)
+    department = db.Column(db.String(100))
+    salary = db.Column(db.String(100))
+    location = db.Column(db.String(100))
+    hiring_manager = db.Column(db.String(100))
+    date_posted = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
---
+++ app/routes/jobs.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask_login import login_required
+from app.util.security import admin_permission
+from app.db import db
+from app.db.models import Job
+from app.util.s3 import conn
+from app.db.util import paginate
+
+job_blueprint = Blueprint('job_blueprint',
+                          __name__, url_prefix='/jobs')
+
+
+@job_blueprint.route('/', methods=['GET', 'POST'])
+def jobs():
+    is_admin = admin_permission.can()
+    jobs_per_page = 9
+    if request.method == 'POST':
+        page = (request.form.get('page_number', 1)) 
+    else:
+        page =1
+    
+    jobs = paginate(Job, page=page, key='job_title', pages=jobs_per_page)
+    
+    return render_template('jobs/jobs.html', is_admin=is_admin,
+                           jobs=jobs, page=page,
+                           primary_title='Jobs')
+
+@job_blueprint.route('/<job_id>')
+def job(job_id):
+    is_admin = admin_permission.can()
+    job = Job.query.filter_by(id=job_id).first()
+
+    return render_template('jobs/job.html', is_admin=is_admin, job=job, page=1,
+                           primary_title=job.job_title)
+
+@job_blueprint.route('/create', methods=['GET','POST'])
+@login_required
+@admin_permission.require()
+def create_job():
+    if request.method == 'POST':
+        job_title = request.form.get('job_title')
+        short_description = request.form.get('short_description')
+        long_description = request.form.get('long_description')
+        department = request.form.get('department')
+        salary = request.form.get('salary')
+        location = request.form.get('location')
+        hiring_manager = request.form.get('hiring_manager')
+
+        job = Job(job_title=job_title, short_description=short_description,
+                  long_description=long_description, department=department,
+                  salary=salary, location=location,hiring_manager=hiring_manager)
+
+        db.session.add(job)
+        db.session.commit()
+
+        return redirect(url_for('job_blueprint.jobs'))
+
+    return render_template('jobs/job_create.html', primary_title='Create Job')
+
+@job_blueprint.route('/edit/<job_id>', methods=['GET', 'POST'])
+@login_required
+@admin_permission.require()
+def edit_job(job_id):
+    job = Job.query.filter_by(id=job_id).first()
+
+    if request.method == 'POST':
+        job_title = request.form.get('job_title')
+        short_description = request.form.get('short_description')
+        long_description = request.form.get('long_description')
+        department = request.form.get('department')
+        salary = request.form.get('salary')
+        location = request.form.get('location')
+        hiring_manager = request.form.get('hiring_manager')
+
+        db.session.commit()
+
+        return redirect(url_for('job_blueprint.job', job_id=id))
+    
+    return render_template('jobs/job_edit.html', job=job, primary_title='Edit Job')
+
+
+@job_blueprint.route('/delete/<job_id>', methods=['POST'])
+@login_required
+@admin_permission.require()
+def delete_job(job_id):
+    job = Job.query.filter_by(id=job_id).first()
+    
+    db.session.delete(job)
+    db.session.commit()
+
+    return redirect(url_for('job_blueprint.jobs'))
