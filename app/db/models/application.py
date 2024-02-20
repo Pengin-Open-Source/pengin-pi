ea44447287d232779341e295584b2b6ee08b178b
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-20 09:07:27 -0500

    Resolve InvalidRequestError caused by issue with SQLAlchemy mappings

--- app/db/models/application.py
+++ app/db/models/application.py
@@ -1,11 +1,10 @@
 from app.db import db
 from app.util.uuid import id
-from .job import Job
+from app.db.models.job import Job
 
-class Application(db.Model): #399-application-crm
+class Application(db.Model):
     __tablename__ = 'application'
     id = db.Column(db.String(36), default=id, primary_key=True)
-    job_id = db.Column(db.String(36), db.ForeignKey(Job.id)) #matches the id of the job for which the applicant is applying
     message_to_hiring_manager = db.Column(db.Text)
     date_applied = db.Column(db.String(100))
     location_of_candidate = db.Column(db.String(100))
