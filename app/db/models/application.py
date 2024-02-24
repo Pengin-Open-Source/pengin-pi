36a6811e3edbb8cd1c78c290eb1f16622a0280ef
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-24 10:27:49 -0500

    Change date_applied column type to DateTime in Application table

--- app/db/models/application.py
+++ app/db/models/application.py
@@ -9,4 +9,4 @@ class Application(db.Model):
     cover_letter_path = db.Column(db.String(100))
     message = db.Column(db.Text)
     location = db.Column(db.String(100))
-    date_applied = db.Column(db.String(100))
+    date_applied = db.Column(db.DateTime(timezone=True), nullable=True)
