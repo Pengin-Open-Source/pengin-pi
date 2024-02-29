88b2a649ec61967a9318cb82cb93561e82c87bd7
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-29 09:59:44 -0500

    Update Application ORM to add status codes, status code date change, and methods to handle logic of status code changes

--- app/db/models/application.py
+++ app/db/models/application.py
@@ -1,7 +1,14 @@
 from app.db import db
 from app.util.uuid import id
 from app.db.models.job import Job
-
+from enum import Enum
+from datetime import datetime
+class ApplicationStatusCode(Enum):
+    PENDING = 'pending' # default status when application is created
+    MAYBE = 'maybe' # admin can change to this after reviewing application
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+    DELETED = 'deleted' # deletes from view but keeps in database
 class Application(db.Model):
     __tablename__ = 'application'
     id = db.Column(db.String(36), default=id, primary_key=True)
@@ -10,3 +17,22 @@ class Application(db.Model):
     message = db.Column(db.Text)
     location = db.Column(db.String(100))
     date_applied = db.Column(db.DateTime(timezone=True), nullable=True)
+    status_code = db.Column(db.Enum(ApplicationStatusCode), default=ApplicationStatusCode.PENDING)
+    status_code_date_change = db.Column(db.DateTime(timezone=True), nullable=True)
+
+    # Methods to handle application status code changes
+    def reject_application(self):
+        self.status_code = ApplicationStatusCode.REJECTED
+        self.status_code_date_change = datetime.now()
+
+    def accept_application(self):
+        self.status_code = ApplicationStatusCode.ACCEPTED
+        self.status_code_date_change = datetime.now()
+    
+    def maybe_application(self):
+        self.status_code = ApplicationStatusCode.MAYBE
+        self.status_code_date_change = datetime.now()
+    
+    def delete_application(self):
+        self.status_code = ApplicationStatusCode.DELETED
+        self.status_code_date_change = datetime.now()
