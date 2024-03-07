99edc5579445fe2389527c7178710f118619bc63
franfif <francis.barrow@gmail.com>
2024-03-06 19:42:28 -0500

    Return only the last n messages when loading the chat room

--- app/routes/message.py
+++ app/routes/message.py
@@ -5,20 +5,22 @@ from flask_login import login_required
 
 chat_blueprint = Blueprint("chat_blueprint", __name__, url_prefix="/chat")
 
+DEFAULT_MESSAGES_TO_LOAD = 15
+
 
 @chat_blueprint.route("/get_past_messages/<room_id>/")
 @login_required
 def get_past_messages(room_id):
-    room = Room.query.get(room_id)
+    room = Room.query.get_or_404(room_id)
 
-    # TODO paginate messages
-    past_messages = []
-    for message in room.messages:
-        past_messages.append(
-            {
-                "author_name": message.author.name,
-                "content": message.content,
-                "timestamp": message.timestamp,
-            }
-        )
+    def serialize_message(message):
+        return {
+            "author_name": message.author.name,
+            "content": message.content,
+            "timestamp": message.timestamp,
+        }
+
+    past_messages = room.messages[-DEFAULT_MESSAGES_TO_LOAD:]
+    past_messages = tuple(map(serialize_message, past_messages))
+
     return jsonify(past_messages)
