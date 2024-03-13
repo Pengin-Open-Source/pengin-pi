c3ed28e4af1916f74ad4e3d190c299a1a1be4247
franfif <francis.barrow@gmail.com>
2024-03-13 13:56:02 -0400

    Clean code with Black PEP8 recommendations

--- app/routes/about.py
+++ app/routes/about.py
@@ -7,8 +7,8 @@ from app.db.models import About
 from app.util.s3 import conn
 from werkzeug.utils import secure_filename
 
-about_blueprint = Blueprint('about_blueprint', __name__,
-                            url_prefix="/about")
+
+about_blueprint = Blueprint("about_blueprint", __name__, url_prefix="/about")
 
 
 @about_blueprint.route("/")
@@ -21,13 +21,18 @@ def view():
         image = "/static/images/test.png"
 
     if about:
-        logging.info('Image S3 URL accessed:' + about.image)
+        logging.info("Image S3 URL accessed:" + about.image)
 
-    return render_template('about/about_main.html', about=about,
-                           is_admin=is_admin, image=image, primary_title='About Us')
+    return render_template(
+        "about/about_main.html",
+        about=about,
+        is_admin=is_admin,
+        image=image,
+        primary_title="About Us",
+    )
 
 
-@about_blueprint.route('/edit', methods=['GET', 'POST'])
+@about_blueprint.route("/edit", methods=["GET", "POST"])
 @login_required
 @admin_permission.require()
 def edit_about():
@@ -42,27 +47,30 @@ def edit_about():
         except:
             image = "/static/images/test.png"
 
-        if about:
-            logging.info('Image S3 URL accessed:' + about.image)
+        logging.info("Image S3 URL accessed:" + about.image)
 
-        if request.method == 'POST':
-            about.name = request.form.get('name')
-            about.article = request.form.get('article')
-            about.facebook = request.form.get('facebook')
-            about.instagram = request.form.get('instagram')
-            about.whatsapp = request.form.get('whatsapp')
-            about.linkedin = request.form.get('linkedin')
-            about.youtube = request.form.get('youtube')
-            about.twitter = request.form.get('twitter')
-            about.phone = request.form.get('phone')
-            about.address1 = request.form.get('address1')
-            about.address2 = request.form.get('address2')
-            about.city = request.form.get('city')
-            about.state = request.form.get('state')
-            about.country = request.form.get('country')
-            about.tags = request.form.get('tags')
+        if request.method == "POST":
+            about.name = request.form.get("name")
+            about.article = request.form.get("article")
+            about.facebook = request.form.get("facebook")
+            about.instagram = request.form.get("instagram")
+            about.whatsapp = request.form.get("whatsapp")
+            about.linkedin = request.form.get("linkedin")
+            about.youtube = request.form.get("youtube")
+            about.twitter = request.form.get("twitter")
+            about.phone = request.form.get("phone")
+            about.address1 = request.form.get("address1")
+            about.address2 = request.form.get("address2")
+            about.city = request.form.get("city")
+            about.state = request.form.get("state")
+            about.country = request.form.get("country")
+            about.tags = request.form.get("tags")
             image = request.files["file"]
-            url = image.filename if "file" in request.files and image.filename != "" else about.image
+            url = (
+                image.filename
+                if "file" in request.files and image.filename != ""
+                else about.image
+            )
             if about.image != url:
                 about.image = url
                 if image:
@@ -73,39 +81,56 @@ def edit_about():
 
             return redirect(url_for("about_blueprint.view"))
 
-        return render_template('about/edit.html', about=about, image=image, primary_title='Edit About Page')
-    elif request.method == 'POST':
-        name = request.form.get('name')
-        article = request.form.get('article')
-        facebook = request.form.get('facebook')
-        instagram = request.form.get('instagram')
-        whatsapp = request.form.get('whatsapp')
-        linkedin = request.form.get('linkedin')
-        youtube = request.form.get('youtube')
-        twitter = request.form.get('twitter')
-        phone = request.form.get('phone')
-        address1 = request.form.get('address1')
-        address2 = request.form.get('address2')
-        city = request.form.get('city')
-        state = request.form.get('state')
-        country = request.form.get('country')
-        tags = request.form.get('tags')
+        return render_template(
+            "about/edit.html", about=about, image=image, primary_title="Edit About Page"
+        )
+    elif request.method == "POST":
+        name = request.form.get("name")
+        article = request.form.get("article")
+        facebook = request.form.get("facebook")
+        instagram = request.form.get("instagram")
+        whatsapp = request.form.get("whatsapp")
+        linkedin = request.form.get("linkedin")
+        youtube = request.form.get("youtube")
+        twitter = request.form.get("twitter")
+        phone = request.form.get("phone")
+        address1 = request.form.get("address1")
+        address2 = request.form.get("address2")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        country = request.form.get("country")
+        tags = request.form.get("tags")
         image = request.files["file"]
-        url = image.filename if "file" in request.files and image.filename != "" else '/static/images/test.png'
+        url = (
+            image.filename
+            if "file" in request.files and image.filename != ""
+        )
         if image:
             image.filename = secure_filename(image.filename)
             url = conn.create(image)
 
-        new_about = About(name=name, article=article, facebook=facebook,
-                            instagram=instagram, whatsapp=whatsapp,
-                            linkedin=linkedin, youtube=youtube, phone=phone,
-                            twitter=twitter, address1=address1,
-                            address2=address2, city=city, state=state,
-                            country=country, tags=tags, image=url)
+        new_about = About(
+            name=name,
+            article=article,
+            facebook=facebook,
+            instagram=instagram,
+            whatsapp=whatsapp,
+            linkedin=linkedin,
+            youtube=youtube,
+            phone=phone,
+            twitter=twitter,
+            address1=address1,
+            address2=address2,
+            city=city,
+            state=state,
+            country=country,
+            tags=tags,
+            image=url,
+        )
 
         db.session.add(new_about)
         db.session.commit()
 
         return redirect(url_for("about_blueprint.view"))
 
-    return render_template('about/create.html', primary_title='Create About Page')
+    return render_template("about/create.html", primary_title="Create About Page")
