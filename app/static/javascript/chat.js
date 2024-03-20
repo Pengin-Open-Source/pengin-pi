3f6dd650cfe34d502a8f87cbf036923753c1eead
franfif <francis.barrow@gmail.com>
2024-03-20 16:46:26 -0400

    Fix chat javascript event handlers

--- app/static/javascript/chat.js
+++ app/static/javascript/chat.js
@@ -3,24 +3,34 @@ const sent_messages = document.querySelector(".message-display-sender");
 const received_messages = document.querySelector(".message-display-receiver");
 let room_id = "";
 
-// Add event listener to the room buttons (to open chat with other users)
+// Add event listener to the user buttons (to open chat with another user)
+const selectUserButtons = $(".btn-select-user");
+for (const button of selectUserButtons) {
+    button.addEventListener("click", function () {
+        const user_id = this.dataset.userId;
+        selectRoom("user", user_id)
+    });
+}
+
+// Add event listener to the room buttons (to open a room already created)
 const selectRoomButtons = $(".btn-select-room");
 for (const button of selectRoomButtons) {
     button.addEventListener("click", function () {
-        const room = this.dataset.room;
-        selectRoom(room)
+        const room_id = this.dataset.roomId;
+        selectRoom("room", room_id)
     });
 }
 
 // Join current user to room with selected room
-function selectRoom(room) {
-    $('div.message-display-sender').empty();
-    $("div.message-display-receiver").empty();
-    sent_messages.content = document.querySelector(".message-display-sender");
-    received_messages.content = document.querySelector(".message-display-receiver");
+function selectRoom(type_of_id, id) {
+    $('div.message-display').empty();
 
-    console.log("about to join room with " + room)
-    socketio.emit("join_room", { other_user: room });
+    if (type_of_id === "user") {
+        data = { user_id: id }
+    } else if (type_of_id === "room") {
+        data = { room_id: id }
+    }
+    socketio.emit("join_room", data);
 }
 
 // Fetch all messages from the server DB
@@ -116,10 +126,9 @@ socket.on('update chat', function (data) {
 
 // Send message to the server
 function sendMessage() {
-    const message = $("#message")[0];
+    const message = $("#chat_message")[0];
     if (message.value === "") return;
-    const currentUserId = document.getElementById('current-user').dataset.id;
-    socketio.emit("save_message", { author_id: currentUserId, content: message.value, room_id: room_id});
+    socketio.emit("save_message", {content: message.value, room_id: room_id});
     message.value = "";
 }
 
